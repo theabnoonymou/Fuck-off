11117074a015acdf5321346433b6c286d187bfbb
ZephyrusFrost <141444827+ZephyrusFrost@users.noreply.github.com>
2024-03-31 15:57:37 +0800

    Update del.js

--- scripts/cmds/del.js
+++ scripts/cmds/del.js
@@ -23,10 +23,10 @@ role: 2,
     fs.unlink(filePath, (err) => {
       if (err) {
         console.error(err);
-        api.sendMessage(`âŽï¸ | Failed to delete ${fileName}.`, event.threadID);
+        api.sendMessage(`❌ Failed to delete ${fileName}.`, event.threadID);
         return;
       }
-      api.sendMessage(`âœ…ï¸ ( ${fileName} ) Deleted successfully!`, event.threadID);
+      api.sendMessage(`✅『${fileName}』Deleted successfully!`, event.threadID);
     });
   }
 };
